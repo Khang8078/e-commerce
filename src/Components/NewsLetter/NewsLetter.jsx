005f2592daaf75315989249d608328ae0eb4932a
Khang Huynh <khanghuynh8078@gmail.com>
2024-08-21 20:39:45 -0700

    add footer

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NewsLetter.css'
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive Offers on Your Emails</h1>
+        <p>Subscribe to our newsletter and stay update</p>
+        <div>
+            <input type="em" name="" id="" placeholder='Your email id' />
+            <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
