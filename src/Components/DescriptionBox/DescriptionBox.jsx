a641dab43c0f34ad014bd9282c6d14b52e7c64cc
Khang Huynh <khanghuynh8078@gmail.com>
2024-08-25 00:11:05 -0700

    add description box

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform that ...</p>
+            <p>E-commerce websites typically display products or services along with details</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
